a9a5b99ae30f03afdbb78ff002b00ae6ecb78e07
valtat <82716180+valtat@users.noreply.github.com>
2024-02-11 17:58:34 +0200

    Updated app.js to use routes for home, automation and settings pages

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,11 @@
 const express = require("express");
-
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const routes = require("./routes/routes");
+
+app.use("/", routes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log("Server is running on port " + port);
 });
