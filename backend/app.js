3545b2f3642a0e4a85e7d4bfdfde3c32f7232be1
valtat <82716180+valtat@users.noreply.github.com>
2024-02-12 09:18:49 +0200

    Updated app.js to include all the new routes

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const routes = require("./routes/routes");
+const automationRouter = require("./routes/automationRoutes");
+const userRouter = require("./routes/userRoutes");
+const roomRouter = require("./routes/roomRoutes");
+const accessoryRouter = require("./routes/accessoryRoutes");
 
-app.use("/", routes);
+app.use("/automation", automationRouter);
+app.use("/users", userRouter);
+app.use("/rooms", roomRouter);
+app.use("/accessories", accessoryRouter);
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
