e5372db32c0b55fe57ff77ef727bfbda2e1740a2
Valerio Tatananni <valerio.tatananni@gmail.com>
2024-02-17 12:24:56 +0200

    Edit Header.jsx to have animations and prepared it with onClick handlers for future implementations

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,53 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faPlus } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
 import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faPlus,
+  faTimes,
+  faLightbulb,
+  faTachometerAlt,
+  faArrowCircleRight,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+const menuItems = [
+  { icon: faLightbulb, text: "Add accessory", onClick: () => {} },
+  { icon: faTachometerAlt, text: "Add automation", onClick: () => {} },
+  { icon: faArrowCircleRight, text: "Add room", onClick: () => {} },
+  { icon: faUser, text: "Add users", onClick: () => {} },
+];
+
+const MenuItem = ({ icon, text, onClick }) => {
+  return (
+    <div className="menu-item" onClick={onClick}>
+      <FontAwesomeIcon icon={icon} />
+      <span class="hover-underline-animation">{text}</span>
+    </div>
+  );
+};
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="header">
       <div className="header-item home-icon">
         <FontAwesomeIcon icon={faHome} className="header-icon" />
       </div>
-      <div className="header-item plus-icon">
-        <FontAwesomeIcon icon={faPlus} className="header-icon" />
+      <div className="header-item">
+        <div className="plus-icon" onClick={() => setIsOpen(!isOpen)}>
+          <FontAwesomeIcon
+            icon={isOpen ? faTimes : faPlus}
+            className="header-icon"
+          />
+        </div>
+        {isOpen && (
+          <div className="overflow-menu">
+            {menuItems.map((item, index) => (
+              <MenuItem key={index} {...item} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
