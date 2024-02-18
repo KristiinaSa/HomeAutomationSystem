6790cb7af9adc1ab21ca359abaaf58dd75f45339
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2024-02-18 21:18:12 +0200

    Added Create New Automation button and confirmed AutomationForm functionality.

--- frontend/src/components/Automations/Automations.jsx
+++ frontend/src/components/Automations/Automations.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { dummyAutomations } from "../../dummyData/dummyAutomations";
 import { AutomationCard } from "./AutomationCard";
+import { Link } from "react-router-dom";
 
 export const Automations = () => {
   const [automations, setAutomations] = useState([]);
@@ -16,6 +17,9 @@ export const Automations = () => {
       {automations.map((automation) => (
         <AutomationCard key={automation.id} automation={automation} />
       ))}
+      <Link to="/automations/new">
+        <button>Create New Automation</button>
+      </Link>
     </div>
   );
 };
