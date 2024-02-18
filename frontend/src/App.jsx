e80a9be90287e19420df1b3739b165e29667f516
valtat <82716180+valtat@users.noreply.github.com>
2024-02-18 13:05:47 +0200

    Add HomeMobile component to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavbarMobile from "./components/NavbarMobile";
 import Header from "./components/Header";
+import HomeMobile from "./components/HomeMobile";
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <HomeMobile />
         <NavbarMobile />
       </div>
     </>
