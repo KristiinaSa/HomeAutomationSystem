345fcf8ac22f9f1f45642d1f29c54bfe3cc81344
Jonne Karhunen <jonne.karhunen@metropolia.fi>
2024-02-20 23:59:41 +0200

    Removed unnecessary context.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,19 +11,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AutomationForm } from "./components/AutomationForm/AutomationForm";
 import { Automations } from "./components/Automations/Automations";
 
-import { DarkLightProvider } from "./contexts/DarkLightProvider";
-
 function App() {
   return (
-    <DarkLightProvider>
-      <Router>
-        <Routes>
-          <Route path="/automations" element={<Automations />} />
-          <Route path="/automations/new" element={<AutomationForm />} />
-          <Route path="/automations/edit/:id" element={<AutomationForm />} />
-        </Routes>
-      </Router>
-    </DarkLightProvider>
+    <Router>
+      <Routes>
+        <Route path="/automations" element={<Automations />} />
+        <Route path="/automations/new" element={<AutomationForm />} />
+        <Route path="/automations/edit/:id" element={<AutomationForm />} />
+      </Routes>
+    </Router>
   );
 }
 
