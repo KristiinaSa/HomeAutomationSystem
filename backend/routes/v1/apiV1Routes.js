c57821b62e43c2b1ae63a98abc9dd5a5ce45ed08
valtat <82716180+valtat@users.noreply.github.com>
2024-02-23 20:37:02 +0200

    Update apiV1Routes to include login route

--- backend/routes/v1/apiV1Routes.js
+++ backend/routes/v1/apiV1Routes.js
@@ -6,10 +6,12 @@ import automationRouter from "./automationRoutes.js";
 import userRouter from "./userRoutes.js";
 import roomRouter from "./roomRoutes.js";
 import accessoryRouter from "./accessoryRoutes.js";
+import router from "./loginRoutes.js";
 
 apiV1Router.use("/automation", automationRouter);
 apiV1Router.use("/users", userRouter);
 apiV1Router.use("/rooms", roomRouter);
 apiV1Router.use("/accessories", accessoryRouter);
+apiV1Router.use("/login", router);
 
 export default apiV1Router;
