2794124b1fa97ad6bf9bc6790f80427dc56b645f
valtat <82716180+valtat@users.noreply.github.com>
2024-02-23 20:37:36 +0200

    Edit login component to use login functionality

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,11 +17,28 @@ const Login = () => {
     setErrorMessage("");
   }, [email, password]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!email || !password) {
       setErrorMessage("Please fill in all the fields");
       return;
+    }
+
+    const response = await fetch("http://localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      console.log("Login successful!");
+    } else {
+      const errorData = await response.json();
+      setErrorMessage(errorData.message);
     }
   };
 
