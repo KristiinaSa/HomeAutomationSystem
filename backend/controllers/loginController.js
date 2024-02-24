1a54f1bdc5643cdb51e887645ff5f0f680cdfaf0
valtat <82716180+valtat@users.noreply.github.com>
2024-02-24 16:47:40 +0200

    Add logout functionality to loginController (doesn't actually do much yet)

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -26,5 +26,22 @@ export const login = (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.status(200).json({ message: "Logged out." });
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (token) {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          res.status(401).json({ message: "Invalid token" });
+        } else {
+          // TO DO: Add token to blacklist or revocation list here
+          res.status(200).json({ message: "Logged out successfully" });
+        }
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Error logging out" });
+    }
+  } else {
+    res.status(401).json({ message: "No token provided" });
+  }
 };
