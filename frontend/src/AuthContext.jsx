c6e5cc09b798ff0c0eb51512b00a9babfd732477
valtat <82716180+valtat@users.noreply.github.com>
2024-02-24 16:48:28 +0200

    Add logout function to authprovided, that deletes token and sets isLoggedIn state to false

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -5,8 +5,13 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
 
+  const logout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, logout }}>
       {children}
     </AuthContext.Provider>
   );
