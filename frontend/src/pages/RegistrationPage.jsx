59f700e5b1a4445a8054acf5dd52906a336ee827
KristiinaSa <kristiinainkeri@gmail.com>
2024-02-27 14:02:16 +0200

    added the registration page

---
+++ frontend/src/pages/RegistrationPage.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import Registration from "../components/Registration";
+
+const RegistrationPage = () => {
+  return (
+    <div className="registration-form-container">
+      <Registration />
+    </div>
+  );
+};
+
+export default RegistrationPage;
