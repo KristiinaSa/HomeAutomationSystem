01119e1a3a4d0cdb8b61e532b86a7ad2ff0145cc
KristiinaSa <kristiinainkeri@gmail.com>
2024-03-04 13:32:16 +0200

    added room page

---
+++ frontend/src/pages/RoomPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoomInformation from "../components/RoomInformation";
+
+const RoomPage = ({}) => {
+  return (
+    <div>
+      <RoomInformation />
+    </div>
+  );
+};
+
+export default RoomPage;
