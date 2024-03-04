186d613e3f7bd0bd2cca1902c9093888e7fbe4a3
KristiinaSa <kristiinainkeri@gmail.com>
2024-03-04 13:33:25 +0200

    added some additional data for testing purposes

--- frontend/src/RoomContext.jsx
+++ frontend/src/RoomContext.jsx
@@ -1,29 +1,31 @@
-import { createContext, useState, useEffect } from 'react';
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+import { createContext, useState, useEffect } from "react";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
 
 export const RoomContext = createContext();
 
 export const RoomProvider = ({ children }) => {
-    const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState([]);
 
-    useEffect(() => {
-        setRooms([
-            {
-                id: 1,
-                name: "Living room",
-                cards: [{ id: 1, title: "Lights", icon: faLightbulb, count: 2 }],
-            },
-            {
-                id: 2,
-                name: "Bedroom",
-                cards: [{ id: 2, title: "Lights", icon: faLightbulb, count: 1 }],
-            },
-        ]);
-    }, []);
+  useEffect(() => {
+    setRooms([
+      {
+        id: 1,
+        name: "Living room",
+        cards: [
+          { id: 1, title: "Lights", icon: faLightbulb, status: "ON" },
+          { id: 2, title: "Lights", icon: faLightbulb, status: "OFF" },
+          { id: 3, title: "Lights", icon: faLightbulb, status: "ON"}
+        ],
+      },
+      {
+        id: 2,
+        name: "Bedroom",
+        cards: [{ id: 2, title: "Lights", icon: faLightbulb, status: 1 }],
+      },
+    ]);
+  }, []);
 
-    return (
-        <RoomContext.Provider value={{ rooms }}>
-            {children}
-        </RoomContext.Provider>
-    );
-}
+  return (
+    <RoomContext.Provider value={{ rooms }}>{children}</RoomContext.Provider>
+  );
+};
