e4d6a74e8871dc2240aed1c2b7455704a5f93210
KristiinaSa <kristiinainkeri@gmail.com>
2024-03-12 15:16:52 +0200

    removed rooms and added accessories to navbar

--- frontend/src/components/NavbarDesktop.jsx
+++ frontend/src/components/NavbarDesktop.jsx
@@ -1,18 +1,17 @@
 import { useContext } from "react";
-import { CategoriesContext } from "../context/CategoriesContext";
 import { RoomContext } from "../context/RoomContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { faHouseLaptop} from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 import "./NavbarDesktop.css";
 
 const NavbarDesktop = () => {
-  const { categories } = useContext(CategoriesContext);
   const { rooms } = useContext(RoomContext);
   const { isLoggedIn } = useContext(AuthContext);
 
@@ -20,79 +19,85 @@ const NavbarDesktop = () => {
     if (!isLoggedIn) {
       e.preventDefault();
     }
-  }
+  };
 
   return (
     <div className="navbar-desktop">
       <h2 className="logo">Smart Home Mate</h2>
       <div className="navbar-main">
-        <NavLink to="/" className={({ isActive}) => 
-      isActive ? "active" : "navbar-link"}
-      onClick={(e) => {
-        handleClick(e);
-      }}
-      >
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "navbar-link")}
+          onClick={(e) => {
+            handleClick(e);
+          }}
+        >
           <div className="navbar-main_item">
             <FontAwesomeIcon icon={faHome} />
             <p>Home</p>
           </div>
         </NavLink>
-        <NavLink to="/automations" className={({ isActive}) => 
-      isActive ? "active" : "navbar-link"}
-      onClick={(e) => {
-        handleClick(e);
-      }}
-      >
+        <NavLink
+          to="/automations"
+          className={({ isActive }) => (isActive ? "active" : "navbar-link")}
+          onClick={(e) => {
+            handleClick(e);
+          }}
+        >
           <div className="navbar-main_item">
             <FontAwesomeIcon icon={faTachometerAlt} />
             <p>Automation</p>
           </div>
         </NavLink>
-        <NavLink to="/settings" className={({ isActive}) => 
-      isActive ? "active" : "navbar-link"}
-      onClick={(e) => {
-        handleClick(e);
-      }}
-      >
+        <NavLink
+          to="/settings"
+          className={({ isActive }) => (isActive ? "active" : "navbar-link")}
+          onClick={(e) => {
+            handleClick(e);
+          }}
+        >
           <div className="navbar-main_item">
             <FontAwesomeIcon icon={faCog} />
             <p>Settings</p>
           </div>
         </NavLink>
+        <NavLink
+          to="/accessories"
+          className={({ isActive }) => (isActive ? "active" : "navbar-link")}
+          onClick={(e) => {
+            handleClick(e);
+          }}
+        >
+          <div className="navbar-main_item">
+            <FontAwesomeIcon icon={faHouseLaptop} />
+            <p>Accessories</p>
+          </div>
+        </NavLink>
       </div>
       {isLoggedIn && (
         <>
-        <div className="navbar-categories-container">
-        <h3>Categories</h3>
-        <div className="navbar-categories">
-          {categories.map((category) => (
-            <div key={category.id} className="navbar-category">
-              <FontAwesomeIcon icon={category.icon} />
-              <p>{category.title}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="navbar-rooms-container">
-        <h3>Rooms</h3>
-        <div className="navbar-rooms">
-          {rooms.map((room) => (
-            <div key={room.id} className="navbar-room">
-              <NavLink to={`/room/${room.id}`} className={({ isActive }) =>
-                isActive ? "active" : "navbar-link"}
-              >
-                <div className="navbar-room">
-                  <FontAwesomeIcon icon={faArrowAltCircleRight} />
-                  <p>{room.name}</p>
+          <div className="navbar-rooms-container">
+            <h3>Rooms</h3>
+            <div className="navbar-rooms">
+              {rooms.map((room) => (
+                <div key={room.id} className="navbar-room">
+                  <NavLink
+                    to={`/room/${room.id}`}
+                    className={({ isActive }) =>
+                      isActive ? "active" : "navbar-link"
+                    }
+                  >
+                    <div className="navbar-room">
+                      <FontAwesomeIcon icon={faArrowAltCircleRight} />
+                      <p>{room.name}</p>
+                    </div>
+                  </NavLink>
                 </div>
-              </NavLink>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-      </>
+          </div>
+        </>
       )}
-      
     </div>
   );
 };
