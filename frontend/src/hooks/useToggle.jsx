a03b78e611be71eb966b78ebeb314183649a0e8c
valtat <82716180+valtat@users.noreply.github.com>
2024-03-12 12:41:47 +0200

    Refactor useToggle hook to use accessoryServices

--- frontend/src/hooks/useToggle.jsx
+++ frontend/src/hooks/useToggle.jsx
@@ -1,16 +1,13 @@
 import { useContext } from "react";
 import { DeviceContext } from "../context/DeviceContext";
-import { toggleOnOff } from "../services/accessoryServices";
+import accessoryServices from "../services/accessoryServices";
 
 const useToggle = () => {
   const { devices, setDevices } = useContext(DeviceContext);
-  console.log(categories);
-  console.log(rooms);
-  console.log(devices);
 
   const handleToggle = async (id) => {
     try {
-      const device = await toggleOnOff(id);
+      const device = await accessoryServices.toggleOnOff(id);
       console.log("Toggled device:", device);
       const updatedDevices = devices.map((d) =>
         d.id === device.id ? { ...d, value: device.value } : d
