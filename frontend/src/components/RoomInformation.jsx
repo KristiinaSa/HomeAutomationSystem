302902e4e0d7873b7a55b8f49358df82d397ed92
JKK <jonne.karhunen@metropolia.fi>
2024-03-13 23:01:14 +0200

    Made devices listed inside a room toggleable

--- frontend/src/components/RoomInformation.jsx
+++ frontend/src/components/RoomInformation.jsx
@@ -1,60 +1,51 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { RoomContext } from "../context/RoomContext";
 import TestCard from "./TestCard";
 import { useParams } from "react-router-dom";
-import { getRoomDevices } from "../services/accessoryServices";
 import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
 import "./RoomInformation.css";
+import useToggle from "../hooks/useToggle";
+import { DeviceContext } from "../context/DeviceContext";
 
 const RoomInformation = () => {
   const { rooms } = useContext(RoomContext);
   const { id } = useParams();
   const room = rooms.find((room) => room.id === parseInt(id));
-  const [roomDevices, setRoomDevices] = useState([]);
-  const [message, setMessage] = useState("");
+  const { devices, errorMsg } = useContext(DeviceContext);
+  const { handleToggle } = useToggle();
 
-  useEffect(() => {
-    if (room) {
-      try {
-        const fetchRoomDevices = async () => {
-          const devices = await getRoomDevices(room.id);
-          setRoomDevices(devices);
-        };
-        fetchRoomDevices();
-      } catch (error) {
-        console.log(error);
-        setMessage(
-          "Uh-oh! We ran into a snag pulling up your room's devices. Could you try again later?"
-        );
-      }
-    }
-  }, [room]);
+  const roomDevices = room
+    ? devices.filter((device) => device.room_id === room.id)
+    : [];
 
   return (
     <div>
       <div className="room-information-container" data-testid="room-container">
-      {room && (
-        <div key={room.id}>
-          <h2>{room.name}</h2>
-          <div className="room-cards-container">
-          <div className="cards">
-            {roomDevices.map((card) => (
-              <TestCard
-                key={card.id}
-                title={card.name}
-                icon={
-                  (card.type === "light") | (card.type === "Light")
-                    ? faLightbulb
-                    : ""
-                }
-                status={card.status}
-              />
-            ))}
+        {room ? (
+          <div key={room.id}>
+            <h2>{room.name}</h2>
+            <div className="room-cards-container">
+              <div className="cards">
+                {roomDevices.map((card) => (
+                  <TestCard
+                    key={card.id}
+                    title={card.name}
+                    icon={
+                      (card.type === "light") | (card.type === "Light")
+                        ? faLightbulb
+                        : ""
+                    }
+                    status={card.value}
+                    onClick={() => handleToggle(card.id)}
+                  />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      {message && <p>{message}</p>}
+        ) : (
+          <p>Loading room information...</p>
+        )}
+        {errorMsg && <p>{errorMsg}</p>}
       </div>
     </div>
   );
