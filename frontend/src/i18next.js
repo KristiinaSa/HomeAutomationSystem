49872ecf002b9d0767ccd3ab6fa92a74eb814f6a
valtat <82716180+valtat@users.noreply.github.com>
2024-03-23 20:53:12 +0200

    Add i18next configuration file

---
+++ frontend/src/i18next.js
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  en: {
+    translation: require("./public/locales/en/translation.json"),
+  },
+};
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    detection: {
+      order: ["queryString", "cookie"],
+      cache: ["cookie"],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    resources,
+  });
+
+export default i18n;
