9a3444465e92e7d052240e345259702bb04cebb1
valtat <82716180+valtat@users.noreply.github.com>
2024-03-24 15:37:54 +0200

    Refactor Login component to use translated strings

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,9 +6,8 @@ import "./Login.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isReady, setIsReady] = useState(false);
   const { login, errorMessage, loginSuccess, setErrorMessage } = useLogin();
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const emailRef = useRef();
   const errorRef = useRef();
@@ -21,14 +20,6 @@ const Login = () => {
     setErrorMessage("");
   }, [email, password, setErrorMessage]);
 
-  useEffect(() => {
-    const handleResources = () => {
-      setIsReady(true);
-    };
-    const unsubscribe = i18n.on("resourcesLoaded", handleResources);
-    return unsubscribe;
-  }, [i18n]);
-
   const handleSubmit = (e) => {
     e.preventDefault();
     login(email, password);
@@ -36,61 +27,55 @@ const Login = () => {
 
   return (
     <div>
-      {isReady ? (
-        <form className="login-form">
-          <h1>{t("login")}</h1>
-          <label htmlFor="email">Email</label>
-          <input
-            className="login-input"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            ref={emailRef}
-            autoComplete="email"
-            id="email"
-            name="email"
-            required
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            className="login-input"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password"
-            id="password"
-            name="password"
-            required
-          />
-          <button
-            className="login-submit-button"
-            type="submit"
-            onClick={handleSubmit}
-            disabled={loginSuccess}
-          >
-            LOG IN
-          </button>
-          <p className="login-register-router">
-            Don&apos;t have an account? <a href="/register">Register</a>
-          </p>
-          <p
-            ref={errorRef}
-            className={errorMessage ? "error-messagge" : "off-screen"}
-            aria-live="assertive"
-          >
-            {errorMessage}
-          </p>
-          {loginSuccess && (
-            <p className="login-success-message">
-              Log in succesfull! Redirecting to home page...
-            </p>
-          )}
-        </form>
-      ) : (
-        <p>Loading translations...</p>
-      )}
+      <form className="login-form">
+        <h1>{t("login")}</h1>
+        <label htmlFor="email">{t("email")}</label>
+        <input
+          className="login-input"
+          type="email"
+          placeholder={t("email")}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          ref={emailRef}
+          autoComplete="email"
+          id="email"
+          name="email"
+          required
+        />
+        <label htmlFor="password">{t("password")}</label>
+        <input
+          className="login-input"
+          type="password"
+          placeholder={t("password")}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+          id="password"
+          name="password"
+          required
+        />
+        <button
+          className="login-submit-button"
+          type="submit"
+          onClick={handleSubmit}
+          disabled={loginSuccess}
+        >
+          {t("login")}
+        </button>
+        <p className="login-register-router">
+          {t("don't have an account?")} <a href="/register">{t("register")}</a>
+        </p>
+        <p
+          ref={errorRef}
+          className={errorMessage ? "error-messagge" : "off-screen"}
+          aria-live="assertive"
+        >
+          {errorMessage}
+        </p>
+        {loginSuccess && (
+          <p className="login-success-message">{t("login successful")}</p>
+        )}
+      </form>
     </div>
   );
 };
