a44a54a988801498ec2c7d9d3664d67f80f450d2
valtat <82716180+valtat@users.noreply.github.com>
2024-03-24 18:57:46 +0200

    Refactor TestCard component to use react-i18next for status translation

--- frontend/src/components/TestCard.jsx
+++ frontend/src/components/TestCard.jsx
@@ -1,11 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./TestCard.css";
+import { useTranslation } from "react-i18next";
 
 const TestCard = ({ title, icon, status, onClick }) => {
   const iconClass = status === "on" ? "icon-on" : "icon-off";
-  const capitalizedStatus = status
+  /*const capitalizedStatus = status
     ? status.charAt(0).toUpperCase() + status.slice(1)
-    : "";
+    : ""; */
+  const lowercasedStatus = status ? status.toLowerCase() : "";
+
+  const { t } = useTranslation();
 
   return (
     <div className="card" onClick={onClick}>
@@ -13,7 +17,7 @@ const TestCard = ({ title, icon, status, onClick }) => {
         <FontAwesomeIcon icon={icon} size="2x" className={iconClass} />
         <div className="card-column noSelect">
           <p>{title}</p>
-          <p>{capitalizedStatus}</p>
+          <p>{t(lowercasedStatus)}</p>
         </div>
       </div>
     </div>
