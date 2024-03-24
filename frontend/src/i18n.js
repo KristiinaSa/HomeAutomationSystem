d84263fcfcfcce9491b4bd0e327e167760965cd8
valtat <82716180+valtat@users.noreply.github.com>
2024-03-24 16:10:31 +0200

    Imported Japanese translation file

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,10 +3,14 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import translationEN from "./locales/en/translation.json";
+import translationJP from "./locales/jp/translation.json";
 
 const resources = {
   en: {
     translation: translationEN,
+  },
+  jp: {
+    translation: translationJP,
   },
 };
 
