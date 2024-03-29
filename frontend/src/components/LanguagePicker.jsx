6a55668b2398a46b743184fcc4b9d3a172cdb450
elenabli <elena.blinova@metropolia.fi>
2024-03-29 22:50:06 +0200

    add css to language picker in settings

--- frontend/src/components/LanguagePicker.jsx
+++ frontend/src/components/LanguagePicker.jsx
@@ -1,19 +1,20 @@
 import useLanguage from '../hooks/useLanguage';
+import './LanguagePicker.css';
 
 export const LanguagePicker = () => {
   const { languages, selectedLanguage, handleLanguageChange, t } = useLanguage();
 
   return (
-    <>
+    <div className='language-container'>
       <h2>{t('Choose a language')}:</h2>
-      <select value={selectedLanguage} onChange={(event) => handleLanguageChange(event.target.value)}>
+      <select value={selectedLanguage} onChange={(event) => handleLanguageChange(event.target.value)} className='language-picker'>
         {languages.map((language) => (
           <option key={language.id} value={language.code}>
             {language.name}
           </option>
         ))}
       </select>
-    </>
+    </div>
   );
 };
 
