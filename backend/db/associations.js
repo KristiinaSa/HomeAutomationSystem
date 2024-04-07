2f759a9ea0c90c823de7fa74340511052529319a
JKK <jonne.karhunen@metropolia.fi>
2024-04-08 00:52:47 +0300

    Cleaned up unnecessary models and imports

--- backend/db/associations.js
+++ backend/db/associations.js
@@ -11,7 +11,6 @@ const UsageHistory = require("../models/usageHistoryModel.js");
 const ValueType = require("../models/valueTypeModel.js");
 const CurrentValue = require("../models/currentValueModel.js");
 const Language = require("../models/languageModel.js");
-const Translation = require("../models/translationModel.js");
 
 Device.belongsTo(System, { foreignKey: "system_id" });
 Device.belongsTo(Room, { foreignKey: "room_id" });
--- backend/db/createTables.js
+++ backend/db/createTables.js
@@ -11,7 +11,6 @@ const System = require("../models/systemModel.js");
 const TimeAutomation = require("../models/timeAutomationModel.js");
 const User = require("../models/userModel.js");
 const Language = require("../models/languageModel.js");
-const Translation = require("../models/translationModel.js");
 const models = require("../models/transModel.js");
 const EnTranslation = models.EnTranslation;
 const FiTranslation = models.FiTranslation;
