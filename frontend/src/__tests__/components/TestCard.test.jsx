e9961db433acf480ed13ef3a6d494b32f444a8b0
valtat <82716180+valtat@users.noreply.github.com>
2024-04-07 18:45:43 +0300

    Refactor TestCard test to take localisation into account

--- frontend/src/__tests__/components/TestCard.test.jsx
+++ frontend/src/__tests__/components/TestCard.test.jsx
@@ -1,8 +1,10 @@
 /* eslint-env jest */
 import { render, screen } from "@testing-library/react";
+import { I18nextProvider } from "react-i18next";
 import TestCard from "../../components/TestCard.jsx";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCoffee as faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import i18n from "../../i18n.js";
 
 library.add(faLightbulb);
 
@@ -11,12 +13,52 @@ describe("TestCard", () => {
     const mockCardTest = {
       title: "Test Title",
       icon: "lightbulb",
-      count: 2,
+      status: "on",
     };
 
-    render(<TestCard {...mockCardTest} />);
+    render(
+      <I18nextProvider i18n={i18n}>
+        <TestCard {...mockCardTest} />
+      </I18nextProvider>
+    );
 
-    const titleElement = screen.getByText("Test Title");
+    const titleElement = screen.getByText("On");
     expect(titleElement).toBeInTheDocument();
+  });
+
+  it("renders correctly in Japanese", async () => {
+    const mockCardTest = {
+      title: "Test Title",
+      icon: "lightbulb",
+      status: "on",
+    };
+
+    await i18n.changeLanguage("jp");
+    render(
+      <I18nextProvider i18n={i18n}>
+        <TestCard {...mockCardTest} />
+      </I18nextProvider>
+    );
+
+    const statusElement = screen.getByText("オン");
+    expect(statusElement).toBeInTheDocument();
+  });
+
+  it("renders correctly in Finnish", async () => {
+    const mockCardTest = {
+      title: "Test Title",
+      icon: "lightbulb",
+      status: "on",
+    };
+
+    await i18n.changeLanguage("fi");
+    render(
+      <I18nextProvider i18n={i18n}>
+        <TestCard {...mockCardTest} />
+      </I18nextProvider>
+    );
+
+    const statusElement = screen.getByText("Päällä");
+    expect(statusElement).toBeInTheDocument();
   });
 });
