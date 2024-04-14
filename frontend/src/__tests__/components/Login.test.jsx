237ac0a39b7cde8e84aaf18289aed7f7dab0676c
JKK <jonne.karhunen@metropolia.fi>
2024-04-14 23:22:37 +0300

    Fixed login, didn't expand tests to cover language changes.

--- frontend/src/__tests__/components/Login.test.jsx
+++ frontend/src/__tests__/components/Login.test.jsx
@@ -5,17 +5,37 @@ import { AuthContext } from "../../context/AuthContext";
 import { ThemeContext } from "../../context/ThemeContext";
 import { test, expect, vi } from "vitest";
 import { I18nextProvider } from "react-i18next";
-import i18n from "../../i18n.js";
+import i18n from "../../i18n-test.js";
+import { LanguageContext } from "../../context/LanguageContext.jsx";
+
+const languages = [
+  { id: 1, code: "en", name: "English" },
+  { id: 2, code: "jp", name: "日本語" },
+  { id: 3, code: "fi", name: "Suomi" },
+];
+
+const languageContextValue = {
+  languages,
+  selectedLanguage: "en",
+  handleLanguageChange: (newLanguage) => {
+    languageContextValue.selectedLanguage = newLanguage;
+    i18n.changeLanguage(newLanguage);
+  },
+  updateLanguage: vi.fn(),
+  t: i18n.t,
+};
 
 test("renders Login component", async () => {
   render(
     <I18nextProvider i18n={i18n}>
-      <AuthContext.Provider value={{ setIsLoggedIn: vi.fn() }}>
-        <ThemeContext.Provider value={{ setTheme: vi.fn() }}>
-          <Router>
-            <Login />
-          </Router>
-        </ThemeContext.Provider>
+      <AuthContext.Provider value={{ isLoggedIn: false }}>
+        <LanguageContext.Provider value={languageContextValue}>
+          <ThemeContext.Provider value={{ theme: "light" }}>
+            <Router>
+              <Login />
+            </Router>
+          </ThemeContext.Provider>
+        </LanguageContext.Provider>
       </AuthContext.Provider>
     </I18nextProvider>
   );
