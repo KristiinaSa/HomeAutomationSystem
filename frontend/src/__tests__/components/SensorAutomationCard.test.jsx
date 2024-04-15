eab47de76f472193a5fd7ba4a713765ae1ca0946
elenabli <elena.blinova@metropolia.fi>
2024-04-15 23:14:19 +0300

    add languageContext to the sensor automation tests

--- frontend/src/__tests__/components/SensorAutomationCard.test.jsx
+++ frontend/src/__tests__/components/SensorAutomationCard.test.jsx
@@ -1,7 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { LanguageContext } from "../../context/LanguageContext.jsx";
 import { SensorAutomationCard } from "../../components/Automations/SensorAutomationCard.jsx";
 import { test, expect } from "vitest";
+import i18n from "../../i18n-test.js";
+import { I18nextProvider } from "react-i18next";
+
+const languageContextValue = {
+  t: i18n.t,
+};
 
 test("SensorAutomationCard renders correctly", () => {
   const mockAutomation = {
@@ -30,9 +37,13 @@ test("SensorAutomationCard renders correctly", () => {
     ],
   };
   render(
-    <Router>
-      <SensorAutomationCard automation={mockAutomation} />
-    </Router>
+    <I18nextProvider i18n={i18n}>
+      <LanguageContext.Provider value={languageContextValue}>
+        <Router>
+          <SensorAutomationCard automation={mockAutomation} />
+        </Router>
+      </LanguageContext.Provider>
+    </I18nextProvider>
   );
   const cardElement = screen.getByTestId("automation-card");
   expect(cardElement).toBeInTheDocument();
@@ -52,3 +63,4 @@ test("SensorAutomationCard renders correctly", () => {
   const editButtonElement = screen.getByTestId("edit-button");
   expect(editButtonElement).toBeInTheDocument();
 });
+
