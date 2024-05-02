a9104509593ec8a3ae01686f335c902551922864
KristiinaSa <kristiinainkeri@gmail.com>
2024-05-02 18:53:38 +0300

    fixed sonarlint issues

--- frontend/src/components/GoBackButton.jsx
+++ frontend/src/components/GoBackButton.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { NavLink, useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./GoBackButton.css";
@@ -8,36 +8,39 @@ function GoBackButton() {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const handleKeyDown = (event) => {
+    if (location.pathname === "/" || location.pathname === "/login" || location.pathname === "/register") {
+      return;
+    }
+    if (event.key === 'Escape') {
+      navigate(-1);
+    }
+  };
+  
   useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (location.pathname === "/" || location.pathname === "/login" || location.pathname === "/register") {
-        return;
-      }
-      if (event.key === 'Escape') {
-        navigate(-1);
-      }
-    };
-
     window.addEventListener('keydown', handleKeyDown);
 
     // Clean up the event listener when the component unmounts
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [navigate]);
+  }, [navigate, location]);
+
 
   if (location.pathname === "/" || location.pathname === "/login" || location.pathname === "/register") {
     return null;
   }
 
   return (
-    <div
+    <button
       onClick={() => navigate(-1)}
+      onKeyDown= {handleKeyDown}
       className="go-back-button"
       data-testid="go-back-button-test"
+      
     >
       <FontAwesomeIcon icon={faArrowCircleLeft} className="go-back-icon" />
-    </div>
+    </button>
   );
 }
 
